6efda7579fcab1a51fa6cb1b85cce52ddeaa316a
Matan Damary <matandamary10@gmail.com>
2023-12-24 12:21:43 +0200

    Using shared component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Input from "../shared-components/src/components/Input/Input";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -27,9 +28,10 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
+        <Input />
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
